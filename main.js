ff57dc522f95154e708a945a834ddd9849a1aa45
Alvaro <alvaropfn@gmail.com>
2017-06-02 11:08:36 -0300

    adicao do package.json e validacao no envio do formulario

--- main.js
+++ main.js
@@ -5,153 +5,6 @@
         "label": "Flow 1"
     },
     {
-        "id": "c2faeef7.f0661",
-        "type": "tab",
-        "label": "Flow 2"
-    },
-    {
-        "id": "7cce9d48.8fa294",
-        "type": "ui_base",
-        "theme": {
-            "name": "theme-dark",
-            "lightTheme": {
-                "default": "#0094CE",
-                "baseColor": "#0094CE",
-                "baseFont": "Helvetica Neue",
-                "edited": true,
-                "reset": false
-            },
-            "darkTheme": {
-                "default": "#097479",
-                "baseColor": "#cade42",
-                "baseFont": "Helvetica Neue",
-                "edited": true,
-                "reset": false
-            },
-            "customTheme": {
-                "name": "Untitled Theme 1",
-                "default": "#4B7930",
-                "baseColor": "#cade42",
-                "baseFont": "Helvetica Neue",
-                "reset": false
-            },
-            "themeState": {
-                "base-color": {
-                    "default": "#097479",
-                    "value": "#097479",
-                    "edited": false
-                },
-                "page-titlebar-backgroundColor": {
-                    "value": "#cade42",
-                    "edited": false
-                },
-                "page-backgroundColor": {
-                    "value": "#111111",
-                    "edited": false
-                },
-                "page-sidebar-backgroundColor": {
-                    "value": "#ffffff",
-                    "edited": false
-                },
-                "group-textColor": {
-                    "value": "#dce983",
-                    "edited": false
-                },
-                "group-borderColor": {
-                    "value": "#555555",
-                    "edited": false
-                },
-                "group-backgroundColor": {
-                    "value": "#333333",
-                    "edited": false
-                },
-                "widget-textColor": {
-                    "value": "#eeeeee",
-                    "edited": false
-                },
-                "widget-backgroundColor": {
-                    "value": "#cade42",
-                    "edited": false
-                },
-                "widget-borderColor": {
-                    "value": "#333333",
-                    "edited": false
-                }
-            }
-        },
-        "site": {
-            "name": "Node-RED Dashboard",
-            "hideToolbar": "false",
-            "allowSwipe": "false",
-            "dateFormat": "DD/MM/YYYY",
-            "sizes": {
-                "sx": 48,
-                "sy": 48,
-                "gx": 6,
-                "gy": 6,
-                "cx": 6,
-                "cy": 6,
-                "px": 0,
-                "py": 0
-            }
-        }
-    },
-    {
-        "id": "f2029026.7aa96",
-        "type": "ui_tab",
-        "z": "",
-        "name": "Form de Testes",
-        "icon": "pets",
-        "order": 1
-    },
-    {
-        "id": "cbe49904.9a3bb8",
-        "type": "ui_group",
-        "z": "",
-        "name": "Entrada",
-        "tab": "f2029026.7aa96",
-        "order": 1,
-        "disp": true,
-        "width": "6"
-    },
-    {
-        "id": "c53bcc23.de15e",
-        "type": "ui_group",
-        "z": "",
-        "name": "Erros",
-        "tab": "f2029026.7aa96",
-        "order": 2,
-        "disp": true,
-        "width": "6"
-    },
-    {
-        "id": "d1bb9c9b.01a14",
-        "type": "ui_group",
-        "z": "",
-        "name": "Conclusão",
-        "tab": "f2029026.7aa96",
-        "order": 3,
-        "disp": true,
-        "width": "6"
-    },
-    {
-        "id": "c55c31e5.35a65",
-        "type": "ui_tab",
-        "name": "Tab 2",
-        "icon": "dashboard",
-        "order": 2
-    },
-    {
-        "id": "d8cf7656.a53a58",
-        "type": "ui_group",
-        "z": "",
-        "name": "Group 2",
-        "tab": "c55c31e5.35a65",
-        "order": 1,
-        "disp": true,
-        "width": "6"
-    },
-    {
         "id": "7c14fc82.e4f9f4",
         "type": "ui_date_picker",
         "z": "e08239b7.a99ba8",
@@ -280,7 +133,7 @@
         "name": "taskEnd",
         "label": "Finalização da tarefa",
         "group": "d1bb9c9b.01a14",
-        "order": 0,
+        "order": 1,
         "width": "3",
         "height": "1",
         "passthru": false,
@@ -414,7 +267,7 @@
         "z": "e08239b7.a99ba8",
         "name": "taskSubmit",
         "group": "d1bb9c9b.01a14",
-        "order": 0,
+        "order": 2,
         "width": "3",
         "height": "1",
         "passthru": false,
@@ -478,85 +331,10 @@
         "url": "",
         "tls": "",
         "x": 670,
-        "y": 540,
+        "y": 520,
         "wires": [
             []
         ]
-    },
-    {
-        "id": "106f6e95.30ada1",
-        "type": "inject",
-        "z": "c2faeef7.f0661",
-        "name": "Tick",
-        "topic": "",
-        "payload": "",
-        "repeat": "",
-        "crontab": "*/5 * * * *",
-        "once": false,
-        "x": 110,
-        "y": 180,
-        "wires": [
-            [
-                "57c7d9c0.7f5e58"
-            ]
-        ]
-    },
-    {
-        "id": "57c7d9c0.7f5e58",
-        "type": "http request",
-        "z": "c2faeef7.f0661",
-        "name": "UK Power",
-        "method": "GET",
-        "url": "http://realtimeweb-prod.nationalgrid.com/SystemData.aspx",
-        "tls": "",
-        "x": 250,
-        "y": 180,
-        "wires": [
-            [
-                "49bbf40d.eff1bc"
-            ]
-        ]
-    },
-    {
-        "id": "49bbf40d.eff1bc",
-        "type": "function",
-        "z": "c2faeef7.f0661",
-        "name": "UK Power Demand",
-        "func": "// does a simple text extract parse of the http output to provide an\n// object containing the uk power demand, frequency and time\n\nif (~msg.payload.indexOf('<span')) {\n    var dem = msg.payload.split('Demand:')[1].split(\"MW\")[0];\n    var fre = msg.payload.split('Frequency:')[1].split(\"Hz\")[0];\n\n    msg.payload = {};\n    msg.payload.demand = parseInt(dem.split(\">\")[1].split(\"<\")[0]);\n    msg.payload.frequency = parseFloat(fre.split(\">\")[1].split(\"<\")[0]);\n    \n    msg2 = {};\n    msg2.payload = (msg.payload.frequency >= 50) ? true : false;\n\n    return [msg,msg2];\n}\n\nreturn null;",
-        "outputs": "2",
-        "noerr": 0,
-        "x": 427,
-        "y": 180,
-        "wires": [
-            [
-                "a66224af.549dd8"
-            ],
-            [
-                "24ac0693.19d0ba"
-            ]
-        ]
-    },
-    {
-        "id": "a66224af.549dd8",
-        "type": "debug",
-        "z": "c2faeef7.f0661",
-        "name": "",
-        "active": false,
-        "complete": false,
-        "x": 627,
-        "y": 150,
-        "wires": []
-    },
-    {
-        "id": "24ac0693.19d0ba",
-        "type": "debug",
-        "z": "c2faeef7.f0661",
-        "name": "",
-        "active": false,
-        "complete": false,
-        "x": 628,
-        "y": 221,
-        "wires": []
     },
     {
         "id": "69c810db.6df3d",
@@ -573,60 +351,19 @@
         ]
     },
     {
-        "id": "2fc95775.2647a8",
-        "type": "ui_form",
-        "z": "c2faeef7.f0661",
-        "name": "",
-        "label": "",
-        "group": "d8cf7656.a53a58",
-        "order": 0,
-        "width": 0,
-        "height": 0,
-        "options": [
-            {
-                "label": "Nome do Testador",
-                "value": "text",
-                "type": "text",
-                "required": true
-            },
-            {
-                "label": "Adicionar Testador",
-                "value": "add",
-                "type": "checkbox",
-                "required": false
-            },
-            {
-                "label": "Remover testador",
-                "value": "rem",
-                "type": "checkbox",
-                "required": false
-            }
-        ],
-        "formValue": {
-            "text": "",
-            "add": false,
-            "rem": false
-        },
-        "payload": "",
-        "topic": "",
-        "x": 200,
-        "y": 420,
-        "wires": [
-            []
-        ]
-    },
-    {
         "id": "3a27f0e1.5f557",
         "type": "function",
         "z": "e08239b7.a99ba8",
         "name": "validateDate",
-        "func": "var sheet = flow.get('sheet') || {}\n\nlet date = new Date(msg.payload)\ndate =\"\" + date\ndate = date.substring(4, 15).split(\" \")\n\nlet day = date[1]\nlet mon = date[0]\nlet yea = date[2]\n\n\nswitch(mon/*.toLocaleLowerCase()*/){\n    case \"Jan\": mon = 1 ;   break;\n    case \"Feb\": mon = 2 ;   break;\n    case \"Mar\": mon = 3 ;   break;\n    case \"Apr\": mon = 4 ;   break;\n    case \"May\": mon = 5 ;   break;\n    case \"Jun\": mon = 6 ;   break;\n    case \"Jul\": mon = 7 ;   break;\n    case \"Aug\": mon = 8 ;   break;\n    case \"Sep\": mon = 9 ;   break;\n    case \"Oct\": mon = 1 ;   break;\n    case \"Nov\": mon = 11;   break;\n    case \"Dec\": mon = 12;   break;\n    default   : mon = -1;   break;\n}\n\ndate = day + \"/\" + mon + \"/\" + yea\n\nsheet[\"taskDate\"] = date\nflow.set('sheet', sheet)\n//return date\n",
+        "func": "var sheet = flow.get('sheet') || {}\n\nlet date = new Date(msg.payload)\ndate =\"\" + date\ndate = date.substring(4, 15).split(\" \")\n\nlet day = date[1]\nlet mon = date[0]\nlet yea = date[2]\n\n\nswitch(mon/*.toLocaleLowerCase()*/){\n    case \"Jan\": mon = 1 ;   break;\n    case \"Feb\": mon = 2 ;   break;\n    case \"Mar\": mon = 3 ;   break;\n    case \"Apr\": mon = 4 ;   break;\n    case \"May\": mon = 5 ;   break;\n    case \"Jun\": mon = 6 ;   break;\n    case \"Jul\": mon = 7 ;   break;\n    case \"Aug\": mon = 8 ;   break;\n    case \"Sep\": mon = 9 ;   break;\n    case \"Oct\": mon = 1 ;   break;\n    case \"Nov\": mon = 11;   break;\n    case \"Dec\": mon = 12;   break;\n    default   : mon = -1;   break;\n}\n\ndate = day + \"/\" + mon + \"/\" + yea\n\nsheet[\"taskDate\"] = date\nflow.set('sheet', sheet)\nreturn date\n",
         "outputs": 1,
         "noerr": 0,
         "x": 670,
         "y": 180,
         "wires": [
-            []
+            [
+                "4541711b.2bafe"
+            ]
         ]
     },
     {
@@ -665,8 +402,8 @@
         "active": true,
         "console": "false",
         "complete": "true",
-        "x": 650,
-        "y": 500,
+        "x": 630,
+        "y": 640,
         "wires": []
     },
     {
@@ -702,7 +439,7 @@
         "type": "function",
         "z": "e08239b7.a99ba8",
         "name": "validateReic",
-        "func": "var sheet = flow.get('sheet')||{}\n\nsheet[\"reicNum\"] = msg.payload\n\nflow.set(\"sheet\", sheet)\n\n//return sheet\n\n",
+        "func": "var sheet = flow.get('sheet')||{}\n\nsheet[\"reicNum\"] = msg.payload\n\nflow.set(\"sheet\", sheet)\n\n//return sheet\n\n/*\n    TODO\n    numero maximos de erros com reincidencia\n*/",
         "outputs": 1,
         "noerr": 0,
         "x": 670,
@@ -722,9 +459,7 @@
         "x": 470,
         "y": 500,
         "wires": [
-            [
-                "54293bea.0b9f54"
-            ]
+            []
         ]
     },
     {
@@ -732,15 +467,140 @@
         "type": "function",
         "z": "e08239b7.a99ba8",
         "name": "urlConcat",
-        "func": "////https://docs.google.com/forms/d/e/1FAIpQLSfUa7auk26fvgrF1nDFVGZCCj-RYzCty2w4gmGE5om-M6tmjw/formResponse?usp=pp_url&entry.1544623310=Numero+da+tarefa&entry.603225237=Testador&entry.1353939701=Testada+dia&entry.1045003221=Iniciada+as&entry.650104975=Dura%C3%A7%C3%A3o+estimada&entry.1842236038=Numero+de+erros&entry.300946219=Numero+de+erros+no+caminho+feliz&entry.835313771=Numero+de+erros+reincidentes&entry.958237353={{topic}}\nlet sheet = flow.get('sheet')\nlet url = \"https://docs.google.com/forms/d/e/1FAIpQLSfUa7auk26fvgrF1nDFVGZCCj-RYzCty2w4gmGE5om-M6tmjw/formResponse?usp=pp_url\"\nurl += \"&entry.1544623310=\"     +sheet['taskNum']//numero da tarefa\nurl += \"&entry.603225237=\"      +sheet['taskTester']//Testador\nurl += \"&entry.1353939701=\"     +sheet['tasDate']//Testada dia\nurl += \"&entry.1045003221=\"     +sheet['taskStart']//Iniciada as\nurl += \"&entry.650104975=\"      +sheet['taskHunt']//Duração estimada\nurl += \"&entry.1842236038=\"     +sheet['errorNum']//Numero de erros\nurl += \"&entry.300946219=\"      +sheet['hpeNum']//Numero de erros no caminho+feliz\nurl += \"&entry.835313771=\"      +sheet['reicNum']//Numero de erros reincidentes\nurl += \"&entry.958237353=\"      +sheet['taskEnd']//Finalizada as\n\nreturn {\"url\": url, \"method\": \"GET\"};",
-        "outputs": 1,
+        "func": "////https://docs.google.com/forms/d/e/1FAIpQLSfUa7auk26fvgrF1nDFVGZCCj-RYzCty2w4gmGE5om-M6tmjw/formResponse?usp=pp_url&entry.1544623310=Numero+da+tarefa&entry.603225237=Testador&entry.1353939701=Testada+dia&entry.1045003221=Iniciada+as&entry.650104975=Dura%C3%A7%C3%A3o+estimada&entry.1842236038=Numero+de+erros&entry.300946219=Numero+de+erros+no+caminho+feliz&entry.835313771=Numero+de+erros+reincidentes&entry.958237353={{topic}}\nlet sheet = flow.get('sheet')\n\nfunction fixDate()\n{\n    function AddZero(num) {\n        return (num >= 0 && num < 10) ? \"0\" + num : num + \"\";\n    }    \n    \n    var now = new Date();\n    var strDateTime = [AddZero(now.getDate()), \n    AddZero(now.getMonth() + 1), \n    now.getFullYear()].join(\"/\")\n    \n    return strDateTime\n}\n\n\n\nlet url = \"https://docs.google.com/forms/d/e/1FAIpQLSfUa7auk26fvgrF1nDFVGZCCj-RYzCty2w4gmGE5om-M6tmjw/formResponse?usp=pp_url\"\nurl += \"&entry.1544623310=\"     +sheet['taskNum']//numero da tarefa\nurl += \"&entry.603225237=\"      +sheet['taskTester']//Testador\nurl += \"&entry.1353939701=\"     +(sheet['taskDate'] || fixDate())//Testada dia\nurl += \"&entry.1045003221=\"     +sheet['taskStart']//Iniciada as\nurl += \"&entry.650104975=\"      +sheet['taskHunt']          //Duração estimada\nurl += \"&entry.1842236038=\"     +(sheet['errorNum'] || 0)   //Numero de erros\nurl += \"&entry.300946219=\"      +(sheet['hpeNum'] || 0)//Numero de erros no caminho+feliz\nurl += \"&entry.835313771=\"      +(sheet['reicNum'] || 0)//Numero de erros reincidentes\nurl += \"&entry.958237353=\"      +sheet['taskEnd']//Finalizada as\n\nif(sheet['topic'])\n    return [{\"url\": url, \"method\": \"GET\"}, null];\nelse\n    return [null, {payload: \"Preencha os campos restantes\"}];",
+        "outputs": "2",
         "noerr": 0,
         "x": 460,
         "y": 540,
         "wires": [
             [
                 "f9f17234.5a23b"
+            ],
+            [
+                "f59d670e.e455a8"
             ]
         ]
+    },
+    {
+        "id": "665164b4.76e26c",
+        "type": "ui_button",
+        "z": "e08239b7.a99ba8",
+        "name": "testSubmit",
+        "group": "d8cf7656.a53a58",
+        "order": 2,
+        "width": "3",
+        "height": "1",
+        "passthru": false,
+        "label": "submit",
+        "color": "",
+        "bgcolor": "",
+        "icon": "check",
+        "payload": "",
+        "payloadType": "str",
+        "topic": "",
+        "x": 310,
+        "y": 620,
+        "wires": [
+            [
+                "e74d4c87.69836"
+            ]
+        ]
+    },
+    {
+        "id": "e74d4c87.69836",
+        "type": "function",
+        "z": "e08239b7.a99ba8",
+        "name": "test",
+        "func": "function fixDate()\n{\n    function AddZero(num) {\n        return (num >= 0 && num < 10) ? \"0\" + num : num + \"\";\n    }    \n    \n    var now = new Date();\n    var strDateTime = [AddZero(now.getDate()), \n    AddZero(now.getMonth() + 1), \n    now.getFullYear()].join(\"/\")\n    \n    return strDateTime\n}\n\n\n\n\n\n\nreturn {\"payload\": fixDate()}",
+        "outputs": 1,
+        "noerr": 0,
+        "x": 450,
+        "y": 620,
+        "wires": [
+            []
+        ]
+    },
+    {
+        "id": "4541711b.2bafe",
+        "type": "debug",
+        "z": "e08239b7.a99ba8",
+        "name": "debugDate",
+        "active": true,
+        "console": "false",
+        "complete": "true",
+        "x": 830,
+        "y": 180,
+        "wires": []
+    },
+    {
+        "id": "f59d670e.e455a8",
+        "type": "ui_toast",
+        "z": "e08239b7.a99ba8",
+        "position": "top left",
+        "displayTime": "4",
+        "highlight": "red",
+        "outputs": 0,
+        "ok": "OK",
+        "cancel": "error",
+        "topic": "",
+        "name": "falha",
+        "x": 650,
+        "y": 560,
+        "wires": []
+    },
+    {
+        "id": "cbe49904.9a3bb8",
+        "type": "ui_group",
+        "z": "",
+        "name": "Entrada",
+        "tab": "f2029026.7aa96",
+        "order": 1,
+        "disp": true,
+        "width": "6"
+    },
+    {
+        "id": "c53bcc23.de15e",
+        "type": "ui_group",
+        "z": "",
+        "name": "Erros",
+        "tab": "f2029026.7aa96",
+        "order": 2,
+        "disp": true,
+        "width": "6"
+    },
+    {
+        "id": "d1bb9c9b.01a14",
+        "type": "ui_group",
+        "z": "",
+        "name": "Conclusão",
+        "tab": "f2029026.7aa96",
+        "order": 3,
+        "disp": true,
+        "width": "6"
+    },
+    {
+        "id": "d8cf7656.a53a58",
+        "type": "ui_group",
+        "z": "",
+        "name": "Group 2",
+        "tab": "c55c31e5.35a65",
+        "order": 1,
+        "disp": true,
+        "width": "6"
+    },
+    {
+        "id": "f2029026.7aa96",
+        "type": "ui_tab",
+        "z": "",
+        "name": "Form de Testes",
+        "icon": "pets",
+        "order": 1
+    },
+    {
+        "id": "c55c31e5.35a65",
+        "type": "ui_tab",
+        "name": "Tab 2",
+        "icon": "dashboard",
+        "order": 2
     }
 ]
